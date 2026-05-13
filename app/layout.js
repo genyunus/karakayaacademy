563de76589e664556dc92386e161462270a77324
genyunus <yunusemregenes@gmail.com>
2026-05-13 18:16:14 -0400

    feat(site): add vercel analytics

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react";
 
 export const metadata = {
   title: "Karakayaacademy | Boxing x Functional Training",
@@ -9,7 +10,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
